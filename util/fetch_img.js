2d9b4b0af0568e8a0627212738ed0043c6d2b84f
fengmiaosen <fengmiaosen@gmail.com>
2016-11-26 17:07:30 +0800

    抓取bing图片

--- util/fetch_img.js
+++ util/fetch_img.js
@@ -9,15 +9,17 @@ let moment = require('moment');
 const source = 'cn';
 const filePrefix = source + '_bing';
 const temp = 'tmp';
+const pathObj = path.parse(__dirname);
 
 module.exports = {
     get(imgSrc) {
         fetch(imgSrc).then(res => {
             let extType = path.extname(imgSrc);
-            let time = moment().format("YYYY-MM-DD-HH-mm");
+            let time = moment().format("YYYY-MM-DD-HH");
             let fileName = filePrefix + '_' + time + extType;
-            let filePath = path.join(path.dirname(path.basename(__dirname)), temp, fileName);
+            let filePath = path.join(pathObj.dir, temp, fileName);
 
+            console.log('path parse:', path.parse(__dirname));
             console.log('path:', filePath);
 
             let dest = fs.createWriteStream(filePath);
